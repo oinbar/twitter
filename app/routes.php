b7726e9f1fbc2cb1a6c4584bf72ebfd9f0464e91
Orr Inbar <inbar.orr@gmail.com>
2014-07-30 20:31:01 -0400

    testing

--- app/routes.php
+++ app/routes.php
@@ -158,8 +158,43 @@ Route::get('/debug', function() {
 });
 
 Route::get('/test', function(){
+		$feed_id = 'israel_feed';
 
+		require_once 'twitter-api-php/TwitterAPIExchange.php';
+		$settings = array(
+		    'oauth_access_token' => "2492151342-mRMDlwJGaij2yZQB5CHyU2FbaymXnIcEhYnhcgC",
+		    'oauth_access_token_secret' => "sDCCPbYt39Uii76de2HcSMbcTFffby1BwxjAEheL6b4dk",
+		    'consumer_key' => "x393VwuVLnnixX6Ld7panxSp8",
+		    'consumer_secret' => "qglHdDR9gcwpyhdFSF37hPpMwXSrIchkmp9DV8TZ8iOzLNt95u"
+		);
+		// GET THE SEARCH CRITERIA FROM THE DB TO ADD INTO THE QUERY
+		$getfield = '?count=100&q=israel' . urlencode(DB::collection('data1')->where('_id', $feed_id)->first()['feed_criteria']);
 
-});
+		$url = 'https://api.twitter.com/1.1/search/tweets.json';
+		$requestMethod = 'GET';
+		$twitter = new TwitterAPIExchange($settings);
+		$json = $twitter->setGetfield($getfield)
+		             ->buildOauth($url, $requestMethod)
+		             ->performRequest();	             
+		
+		// LOOP OVER THE RESULTS COLLECTION
+		$data = json_decode($json, true);
+
+		foreach ($data['statuses'] as $status){		
+			//ADD MONGOID
+			$status['_id'] = $status['id'];
+			$db_record = DB::collection('data1')->where('_id', $status['_id'])->first();	
+			if ($db_record) {
+				// ADD REFERENCE TO FEED AND UPDATE RECORD
+				array_push($db_record['feeds'], $feed_id);
+				$db_record['feeds'] = array_unique($db_record['feeds']);
+				DB::collection('data1')->where('_id', $status['_id'])->update($db_record);
+			} else {						
+				// ADD REFERENCE TO FEED AND INSERT RECORD INTO DB
+				$status['feeds'] = array($feed_id);			
+				DB::collection('data1')->insert($status);	
+			}
+		}
+	});
 
 ?>
