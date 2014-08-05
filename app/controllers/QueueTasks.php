dd4e9c9925a10a9de58a1cbde6bfb453db866ec3
Orr Inbar <inbar.orr@gmail.com>
2014-08-05 10:27:22 -0400

    minor bug fixes

--- app/controllers/QueueTasks.php
+++ app/controllers/QueueTasks.php
@@ -6,29 +6,14 @@ class QueueTasks {
 	public function send_search_query($job, $data){
 
 		$feed_status = DB::connection('mysql')->table('users_feeds')->where('feed_id', $data['feed_id'])->first()->feed_status;
-
-		error_log('feed_status '. $feed_status, 3, '~/app-root/logs/php.log')
-
-
+		
 		if ($feed_status == 'on') {
-
-			error_log('feed status check ok' , 3, '~/app-root/logs/php.log');
-
-			try{
-				$t = new TwitterController();
-				$t->send_search_query($data['feed_id']);
-
-			catch (Exception $e) {
-				error_log($e , 3, '~/app-root/logs/php.log');
-			}
-
+			$t = new TwitterController();
+			$t->send_search_query($data['feed_id']);
 			$job->release(10);
-			} 
-		else {
+		} else {
 			$job->delete();
 		}
-		error_log('job released' , 3, '~/app-root/logs/php.log');
-		}	
 	}
 
 	public function simple ($job, $data){
