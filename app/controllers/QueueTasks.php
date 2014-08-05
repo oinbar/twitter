ef1f451d4ffd65626a79faebad4fc847d5dbf415
Orr Inbar <inbar.orr@gmail.com>
2014-08-05 11:59:06 -0400

    server debugging

--- app/controllers/QueueTasks.php
+++ app/controllers/QueueTasks.php
@@ -3,31 +3,29 @@
 include "TwitterController.php";
 
 class QueueTasks {
-	public function send_search_query($job, $data){
-
-		error_log('this is a test');
-
-		$feed_status = DB::connection('mysql')->table('users_feeds')->where('feed_id', $data['feed_id'])->first()->feed_status;
-		
-		if ($feed_status == 'on') {
-			$t = new TwitterController();
-			$t->send_search_query($data['feed_id']);
-			$job->release(10);
-		} else {
-			$job->delete();
-		}
-	}
-
 	// public function send_search_query($job, $data){
 
 	// 	error_log('this is a test');
 
 	// 	$feed_status = DB::connection('mysql')->table('users_feeds')->where('feed_id', $data['feed_id'])->first()->feed_status;
 		
-		
-	// 	$t = new TwitterController();
-	// 	$t->send_search_query($data['feed_id']);
-	// 	$job->release(10);
-	
+	// 	if ($feed_status == 'on') {
+	// 		$t = new TwitterController();
+	// 		$t->send_search_query($data['feed_id']);
+	// 		$job->release(10);
+	// 	} else {
+	// 		$job->delete();
+	// 	}
 	// }
+
+	public function send_search_query($job, $data){		
+
+		$feed_status = DB::connection('mysql')->table('users_feeds')->where('feed_id', $data['feed_id'])->first()->feed_status;
+		
+		
+		$t = new TwitterController();
+		$t->send_search_query($data['feed_id']);
+		$job->release(10);
+	
+	}
 }
