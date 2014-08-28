9d20017724e6f7d8e7b0984385d3d84963632d67
Orr Inbar <inbar.orr@gmail.com>
2014-08-28 19:09:04 -0400

    debug

--- app/controllers/ProcessingTasks.php
+++ app/controllers/ProcessingTasks.php
@@ -28,11 +28,9 @@ class ProcessingTasks extends BaseController {
 				$since_id = '&since_id=' . $since_id;
 			}			
 
-			$getfield = '?count=50&q=' . urlencode(DB::connection('mysql')
+			$getfield = '?count=50' . $since_id . '&q=' . urlencode(DB::connection('mysql')
 											->table('feeds')->where('id', $feed_id)->orderBy('created_at', 'desc')
 											->first()->criteria);
-
-			Log::error('GETFIELD ' . $getfield);
 
 			// $getfield = '?count=50&q=protest';
 
