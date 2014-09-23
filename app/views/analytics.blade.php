53974b8ce22820e6c04b9d23cc0ad6f689d62e43
Orr Inbar <inbar.orr@gmail.com>
2014-09-23 15:30:47 -0400

    ui1-work

--- app/views/analytics.blade.php
+++ app/views/analytics.blade.php
@@ -6,12 +6,12 @@
    <h1 class="page-header">Feed: Protest > Analytics </h1>
    <p class="lead"><i>Here's whats going on in your neck of the woods:</i></p>
    
-   <p class="text-muted">The hourly trend over the past 24 hours:</p>  
+   <p class="lead">The hourly trend over the past 24 hours:</p>  
    <img src="/images/{{ $protest_hour_trend }}" alt="analytics_img_hour">
    
 
+   <p class="lead">The daily trend over the past 7 days:</p>  
    <img src="/images/{{ $protest_day_trend }}" alt="analytics_img_day">
-   <span class="text-muted">The daily trend over the past 7 days:</span>
 
 
 
