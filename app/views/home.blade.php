0ab431b0b074c2c56eed0155d5fbad5425edb9ad
Orr Inbar <inbar.orr@gmail.com>
2014-09-23 15:52:04 -0400

    ui1-work

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,41 +2,43 @@
 
 @section('main-content')
 
-<div class="pull-left"><h3>Does your organization have a stake in social justice?</h3></div>
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+	<div class="pull-left"><h3>Does your organization have a stake in social justice?</h3></div>
 
-<div class="pull-right"><h3>Take the Tour...</h3></div><br>
+	<div class="pull-right"><h3>Take the Tour...</h3></div><br>
 
-<div class="pull-left"><h3>Are you trying to make a difference?</h3><br>
+	<div class="pull-left"><h3>Are you trying to make a difference?</h3><br>
 
-<h3>Let us help you.</h3><br>
+	<h3>Let us help you.</h3><br>
 
-<h4>What we do:</h4>
-<p>We, at Up2Data, collect time sensitive data across the social web and draw ongoing mission-critical insights tailored to your organization.</p><br>
+	<h4>What we do:</h4>
+	<p>We, at Up2Data, collect time sensitive data across the social web and draw ongoing mission-critical insights tailored to your organization.</p><br>
 
-<h4>How we do it:</h4>
+	<h4>How we do it:</h4>
 
-<h5>1. UNDERSTAND</h5>
-	<p>Our team of software engineers, data scientists and business analysts get together with the people who drive your organization. Together 
-	we shed light on your strategic needs, as we build context and parametrize the problem. <p>
+	<h5>1. UNDERSTAND</h5>
+		<p>Our team of software engineers, data scientists and business analysts get together with the people who drive your organization. Together 
+		we shed light on your strategic needs, as we build context and parametrize the problem. <p>
 
-<h5>2. MODEL</h5>
- <p>We design an analytical model and an underlying data collection engine. The result is a big data system geared towards answering your specific,
- 	goal driven questions.
+	<h5>2. MODEL</h5>
+	 <p>We design an analytical model and an underlying data collection engine. The result is a big data system geared towards answering your specific,
+	 	goal driven questions.
 
-<h5>3. COLLECT</h5>
-<p>We use open source and propietary big data technologies to crawl sites such as twitter, facebook, university newspapers and other sources
-in order to consrtruct a comprehensive picture of the people and events that comprise your social arena.<p>
+	<h5>3. COLLECT</h5>
+	<p>We use open source and propietary big data technologies to crawl sites such as twitter, facebook, university newspapers and other sources
+	in order to consrtruct a comprehensive picture of the people and events that comprise your social arena.<p>
 
-<h5>4. ANALYZE</h5>
-<p>We normalize, mash up, and combine the data into a uniform database. We then use techniques from social network analysis, natural language processing, 
-machine learning, and some good old common sense to derive meaning from the data. We discover influencial individuals, predict future events, uncover 
-trends and explain behaviors, all in a way that allows your organization to maximize its goals.<p>
+	<h5>4. ANALYZE</h5>
+	<p>We normalize, mash up, and combine the data into a uniform database. We then use techniques from social network analysis, natural language processing, 
+	machine learning, and some good old common sense to derive meaning from the data. We discover influencial individuals, predict future events, uncover 
+	trends and explain behaviors, all in a way that allows your organization to maximize its goals.<p>
 
-<h5>5. REPORT & ALERT</h5>
-<p>Once the first batch of data is in and fully analyzed, we deliver an intuitive report that allows you to act on our insights.
-More importantly, by this point we have a built a full data collection and analysis platform that is speficially tailored to your needs, and capable of
-delivering those same insights in real time. The models all update based on incoming data, and we chime in every once in a while
-to give our own interpretation in a fresh report.<p>
+	<h5>5. REPORT & ALERT</h5>
+	<p>Once the first batch of data is in and fully analyzed, we deliver an intuitive report that allows you to act on our insights.
+	More importantly, by this point we have a built a full data collection and analysis platform that is speficially tailored to your needs, and capable of
+	delivering those same insights in real time. The models all update based on incoming data, and we chime in every once in a while
+	to give our own interpretation in a fresh report.<p>
+	</div>
 </div>
 
 @stop
