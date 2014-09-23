0fdf8f960120e4d793819cd69803e52cfb06819b
Orr Inbar <inbar.orr@gmail.com>
2014-09-22 22:49:56 -0400

    ui1-work

--- app/views/analytics.blade.php
+++ app/views/analytics.blade.php
@@ -2,8 +2,10 @@
 
 @section('main-content')
 
+{{ $protest_hour_trend }}
+
 <img src="{{ $protest_hour_trend }}" alt="protest_hour_trend" style="width:304px;height:228px">
-<img src="{{ $protest_day_trend }}" alt="protest_hour_trend" style="width:304px;height:228px">
+<img src="{{ $protest_day_trend }}" alt="protest_day_trend" style="width:304px;height:228px">
 
 
 @stop
