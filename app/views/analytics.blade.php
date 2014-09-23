43894c57b79944d4995cec35f6a0527b5eb07b2a
Orr Inbar <inbar.orr@gmail.com>
2014-09-23 15:15:30 -0400

    ui1-work

--- app/views/analytics.blade.php
+++ app/views/analytics.blade.php
@@ -7,7 +7,7 @@
    <p class="lead"><i>Here's whats going on in your neck of the woods:</i></p>
    <div class="row placeholders">
      <div class="col-xs-6 col-sm-3 placeholder">
-       <img data-src="/images/{{ $protest_hour_trend }}" style="width:600px;height:500px" class="img-responsive" alt="analytics_img">
+       <img data-src="/images/{{ $protest_hour_trend }}" size="width:600px;height:500px" class="img-responsive" alt="analytics_img">
        <h4>Label</h4>
        <span class="text-muted">Something else</span>
      </div>
