63d18fe10c31f311933f1c18446e270aec723476
Orr Inbar <inbar.orr@gmail.com>
2014-09-24 00:31:06 -0400

    ui1-work

--- app/views/adminViews/cache_view.blade.php
+++ app/views/adminViews/cache_view.blade.php
@@ -2,15 +2,18 @@
 
 @section('main-content')
 
-<h3> PendingCalaisList </h3>
-size: {{ $sizePendingCalaisList }}<br><br>
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">           
 
-<h3> PendingSUTimeList </h3>
-size: {{ $sizePendingSUTimeList }}<br><br>
+	<h3> PendingCalaisList </h3>
+	size: {{ $sizePendingCalaisList }}<br><br>
 
-<h3> PendingPersistenceList </h3>
-size: {{ $sizePendingPersistenceList }}<br><br>
+	<h3> PendingSUTimeList </h3>
+	size: {{ $sizePendingSUTimeList }}<br><br>
 
+	<h3> PendingPersistenceList </h3>
+	size: {{ $sizePendingPersistenceList }}<br><br>
+
+<div>
 
 
 
--- app/views/adminViews/mongo_query.blade.php
+++ app/views/adminViews/mongo_query.blade.php
@@ -3,25 +3,31 @@
 
 @section('main-content')
 
-<h2>Mongo Query:</h2>
-{{ Form::open(array('url'=> '/mongoquery/', 
-					'method' => 'GET',
-					'accept-charset' => 'ISO-8859-1')) }}
-{{ Form::label('query', 'type full query: ') }} <br>					
 
-@if ($query)
-	{{ Form::textarea('query', $query) }} <br><br>
-@else
-	{{ Form::textarea('query', 'db.data1.find().toArray()') }} <br><br>
-@endif
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">           
 
-{{form::submit('Submit')}}
+	<h2>Mongo Query:</h2>
+	{{ Form::open(array('url'=> '/mongoquery/', 
+						'method' => 'GET',
+						'accept-charset' => 'ISO-8859-1')) }}
+	{{ Form::label('query', 'type full query: ') }} <br>					
 
-{{ Form::close() }}
+	@if ($query)
+		{{ Form::textarea('query', $query) }} <br><br>
+	@else
+		{{ Form::textarea('query', 'db.data1.find().toArray()') }} <br><br>
+	@endif
 
-<?php
-	
-echo Pre::render($results);
-?>
+	{{form::submit('Submit')}}
+
+	{{ Form::close() }}
+
+	<?php
+		
+	echo Pre::render($results);
+	?>
+
+
+</div>
 
 @stop
