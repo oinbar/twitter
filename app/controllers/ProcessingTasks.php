ecb275269028055016fa098f87883978e8e72189
Orr Inbar <inbar.orr@gmail.com>
2014-09-26 15:22:30 -0400

    ui1-work

--- app/controllers/ProcessingTasks.php
+++ app/controllers/ProcessingTasks.php
@@ -68,7 +68,7 @@ class ProcessingTasks extends BaseController {
 			$data = json_decode($json, true);
 
 			Log::error('TWITTER RESULTS LEN: ' .sizeof($data));
-			Log::error(Pre::render($data));
+			Log::error(implode("\r\n", $data));
 
 			// LOOP OVER THE RESULTS COLLECTION, AND STORE IN CACHE FOR DATA PIPELINE
 			$max_id = 0;
